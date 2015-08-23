46e619f6fc3743090ed2d62c8441b609dda06208
Samir Tambe <samir.tambe1@gmail.com>
2015-08-22 23:16:54 -0400

    Added the (input < 50) part to determine affordability

--- js/filters/CheckFilter.js
+++ js/filters/CheckFilter.js
@@ -3,13 +3,10 @@
     angular.module('Sapp').filter('CheckFilter', function() {
 
         return function(input) {
-
             console.log('CheckFilter: INPUT = '+ input);
-
-            return input ? '\u2713' : '\u2718';
-
+            // if the input is less than 50 dollars per share then return checkmark
+            //else return an X
+            return (input < 50) ? '\u2713' : '\u2718';
         };
-
     });
-
 }());

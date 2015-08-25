9fcc8d975c5bbb38e59578ce94ee5f64c1f6df78
Samir Tambe <samir.tambe1@gmail.com>
2015-08-24 22:27:38 -0400

    Externalized all other parameters to the API call into variables

--- js/services/Services.js
+++ js/services/Services.js
@@ -3,18 +3,20 @@
     angular.module('Sapp')
         .factory('StockListService', ['$resource', function($resource) {
 
-    console.log("Using StockListService");
+            console.log("Using StockListService");
 
-    return $resource(
-'https://www.quandl.com/api/v1/datasets/WIKI/AAPL.json?column=4&trim_start=2015-08-17&trim_end=2015-08-21&auth_token=kA5hVpUMRoQmJyRqFPvk',
+            var symbol = 'AAPL',
+                datatype = '.json?',
+                columns = 'column=4',
+                beginDate = '&trim_start=2015-08-17',
+                endDate = '&trim_end=2015-08-21',
+                API_KEY = '&auth_token=kA5hVpUMRoQmJyRqFPvk';
+
+            return $resource(
+                'https://www.quandl.com/api/v1/datasets/WIKI/' +
+                symbol +datatype + columns + beginDate + endDate + API_KEY,
             { },
-            {
-                query: {
-                    method:'GET',
-                    params: {},
-                    isArray:false
-                }
-            }
+            { query: { method:'GET', params: {}, isArray:false } }
         );
     }])
 

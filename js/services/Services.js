86a16cae4a2e5f780270862bb85e9b9489feb05c
Samir Tambe <samir.tambe1@gmail.com>
2015-09-09 15:16:08 -0400

    Cleaned up code to include Custom Start and End date for view duration for investment symbol

--- js/services/Services.js
+++ js/services/Services.js
@@ -1,25 +1,37 @@
 (function() {
 
     angular.module('Sapp')
-        .factory('StockListService', ['$resource', function($resource) {
-return function(jojo){
+        .factory('StockListService', [
+        '$resource', function($resource) {
+
+            return function(params) {
+                console.log("params.symbol > "+params.symbol);
+                console.log("params.startDate > "+params.startDate);
+                console.log("params.endDate > "+params.endDate);
 
             console.log("Using StockListService");
 
-            var symbol = jojo,//'MSFT',
-                datatype = '.json?',
+            var symbol = datatype = '.json?',
                 columns = 'column=4',
-                beginDate = '&trim_start=2010-09-01',
-                endDate = '&trim_end=2015-08-27',
+//                beginDate = '&trim_start=2010-09-01',
+//                endDate = '&trim_end=2015-08-27',
                 API_KEY = '&auth_token=kA5hVpUMRoQmJyRqFPvk';
 
             return $resource(
                 'https://www.quandl.com/api/v1/datasets/WIKI/' +
-                symbol +datatype + columns + beginDate + endDate + API_KEY,
-            { },
-            { query: { method:'GET', params: {}, isArray:false } }
-        );
-}
+                params.symbol + //investment symbol
+                datatype +
+                columns +
+                '&trim_start=' + params.endDate +
+                '&trim_end=' + params.startDate +
+                API_KEY,
+                { },
+                { query: {
+                    method:'GET',
+                    params: {},
+                    isArray:false }
+                });
+            }//return function
     }])
 
     .factory('WeatherService', ['$resource', function($resource) {

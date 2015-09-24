8bd91eaf05bf180b8c9f2b27905cb8894c60e212
Samir Tambe <samir.tambe1@gmail.com>
2015-09-24 14:22:07 -0400

    Added scope

--- js/services/Services.js
+++ js/services/Services.js
@@ -1,24 +1,24 @@
-angular.module('TambeTech').service('QuandlSvc', ['$http', '$q', function($http, $q) {
+angular.module('TambeTech').service('QuandlSvc', ['$scope', '$http', '$q', function($scope, $http, $q) {
 
     return ({
         getQuandlData: getQuandlData
     });
 
-    function getQuandlData(params) {
+    function getQuandlData() {
 
         var prefix = 'https://www.quandl.com/api/v1/datasets/WIKI/',
             datatype = '.json?',
             columns = 'column=4',
             API_KEY = '&auth_token=kA5hVpUMRoQmJyRqFPvk',
             qnStr = prefix +
-                params.investment.invSymbol +
+                $scope.reqParams.investment.invSymbol +
                 datatype +
 
                 columns +
                 '&trim_start=' +
-                params.endDate +
+                $scope.reqParams.endDate +
                 '&trim_end=' +
-                params.startDate +
+                $scope.reqParams.startDate +
                 API_KEY,
 
             request = $http({
@@ -73,109 +73,3 @@ angular.module('TambeTech').service('QuandlSvc', ['$http', '$q', function($http,
             });//resource
     };// return
 }]);
-
-/*
-    angular.module('cardbycloud')
-        .controller('ContactCtrl',
-                    ['$scope', '$http', 'FormPostSvc','$state',
-                     function ($scope, $http, FormPostSvc, $state ) {
-
-    $scope.data = {flname: '', email: '', phonenum: '', message: ''};
-
-    $scope.submitForm = function(isValid) {
-        if (isValid) {
-            $http({method: 'POST',
-                   url: 'msghandler.php',
-                   transformRequest: FormPostSvc,
-                   data: {
-                       flname: $scope.data.flname,
-                       email: $scope.data.email,
-                       phonenum: $scope.data.phonenum,
-                       message: $scope.data.message
-                  }
-                })
-            .then(function (response) {
-                console.log("THEN: " + response.data);
-
-                var found = response.data.search(/sent/i);
-
-                if (found > -1) {
-                    $state.go('messagesuccess');
-                } else {
-                    $state.go('messageerror');
-                }
-
-                return response;
-
-            },function (response) {
-
-                console.log("CATCH: " +
-                            response.data+' => '+
-                            response.status + ' => '+
-                            response.statusText);
-
-                return response;
-            });
-        }//if
-    };//function
-}]);
-
-
-
-
-
-    angular.module('cardbycloud').factory("FormPostSvc", function() {
-
-// Prepare request data for the form POST.
-
-        function transformRequest( data, getHeaders ) {
-
-            var headers = getHeaders();
-
-            headers['Content-Type'] = "application/x-www-form-urlencoded; charset=utf-8";
-
-            return( serializeData( data ) );
-        }
-
-        // Return the factory value.
-        return( transformRequest );
-
-
-// PRIVATE METHODS.
-// I serialize the given Object into a key-value pair string. This
-// method expects an object and will default to the toString() method.
-
-        function serializeData( data ) {
-
-// If this is not an object, defer to native stringification.
-
-            if (!angular.isObject(data)) {
-                return( ( data == null ) ? "" : data.toString() );
-            }
-
-            var buffer = [];
-
-            // Serialize each key in the object.
-            for ( var name in data ) {
-
-                if (!data.hasOwnProperty(name)) {
-                    continue;
-                }
-
-                var value = data[ name ];
-
-                buffer.push(encodeURIComponent( name ) + "=" +
-                    encodeURIComponent( ( value == null ) ? "" : value )
-                );
-            }//for
-            // Serialize the buffer and clean it up for transportation.
-            var source = buffer.join( "&" ).replace( /%20/g, "+" );
-
-            return(source);
-        }//serialize data
-
-    });//Factory module
-
-}());
-
-*/

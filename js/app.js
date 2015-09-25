2b5e161f458876376c17eea29336692ca4ed6719
Samir Tambe <samir.tambe1@gmail.com>
2015-09-24 23:15:33 -0400

    All States are on one line

--- js/app.js
+++ js/app.js
@@ -4,23 +4,10 @@ angular.module('TambeTech',['ui.router','ngResource']).config(
 
     $urlRouterProvider.otherwise('/home');
 
-    $stateProvider.state('home', {
-        url: '/home',
-        templateUrl: 'views/main_menu.html'
-    })
-    .state('about', {
-        url: '/about',
-        templateUrl: 'views/about.html'
-    })
-    .state('weather', {
-        url: '/weather',
-        templateUrl: 'views/weather.html',
-        controller: 'WeatherCtrl'
-    })
-    .state('finance', {
-        url: '/finance',
-        templateUrl: 'views/finance.html',
-        controller: 'StockListCtrl'
-    });
+    $stateProvider
+    .state('home',{url:'/home',templateUrl:'views/main_menu.html'})
+    .state('about',{url:'/about',templateUrl:'views/about.html'})
+    .state('weather',{url:'/weather',templateUrl:'views/weather.html',controller:'WeatherCtrl'})
+    .state('finance',{url:'/finance',templateUrl:'views/finance.html',controller:'StockListCtrl'});
 
 });

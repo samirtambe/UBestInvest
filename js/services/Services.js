bc40ec02c8359084b88bf2184643a99b75eefdcd
Samir Tambe <samir.tambe1@gmail.com>
2015-09-27 14:11:17 -0400

    made changes to reflect new return object and added NEWS

--- js/services/Services.js
+++ js/services/Services.js
@@ -26,6 +26,9 @@ angular.module('TambeTech').service('HttpSvc', ['$http', '$q', function($http, $
 
         switch(reqType) {
 
+            case 'news':
+                break;
+
             case 'stock':
                 httpObj.url = urls.stock + parm.investment.invSymbol + format;
                 httpObj.params = {
@@ -66,50 +69,55 @@ angular.module('TambeTech').service('HttpSvc', ['$http', '$q', function($http, $
         }//SWITCH
 
 
-            request = $http(httpObj).then(
 
-                // handles SUCCESS
-                function(response) {
+        request = $http(httpObj).then(
 
-                    var retObj;
+            // handles SUCCESS
+            function(response) {
 
-                    switch (reqType) {
+                var retObj;
 
-                        case 'stock':
-                            retObj = response.data.dataset.data;
-                            break;
+                switch (reqType) {
 
-                        case 'dowjones':
-                            retObj = response.data.dataset.data;
-                            break;
+                    case 'news':
+                        retObj = response.data;
+                        break;
 
-                        case 'sp500':
-                            retObj = response.data.dataset.data;
-                            break;
+                    case 'stock':
+                        retObj = response.data.dataset.data;
+                        break;
 
-                        case 'weather':
-                            retObj = response.data;
-                            break;
-                    }
+                    case 'dowjones':
+                        retObj = response.data.dataset.data;
+                        break;
 
-                    return(retObj);
-            },
-                // handles ERROR
+                    case 'sp500':
+                        retObj = response.data.dataset.data;
+                        break;
+
+                    case 'weather':
+                        retObj = response.data;
+                        break;
+                }
+
+                return(retObj);
+        },
+            // handles ERROR
 /* The API response from the server should be returned in a
 normalized format. However, if the request was not handled by the
 server (or what not handles properly - ex. server error), then we
 may have to normalize it on our end, as best we can. */
 
-                function(response ) {
+            function(response ) {
 
-                    if (!angular.isObject(response.data ) ||
-                        !response.data.message) {
-                        return( $q.reject( "QuandlSvc - An unknown error occurred." ) );
-                    }
+                if (!angular.isObject(response.data ) ||
+                    !response.data.message) {
+                    return( $q.reject( "HttpSvc - An unknown error occurred." ) );
+                }
 
 // Otherwise, use expected error message.
-                    return($q.reject(response.data.message));
-                });//THEN
+                return($q.reject(response.data.message));
+            });//THEN
 
         return (request);
     }//GET DATA
@@ -118,15 +126,15 @@ may have to normalize it on our end, as best we can. */
     function getDowJonesData (parm) { return getData('dowjones', parm); }
     function getSP500Data (parm) { return getData('sp500', parm); }
     function getWeatherData (parm) { return getData('weather', parm); }
-    //function getNewsData (parm) { return getData('news', parm); }
+    function getNewsData (parm) { return getData('news', parm); }
 
 
     return ({
         getStockData: getStockData,
         getDowJonesData: getDowJonesData,
         getSP500Data: getSP500Data,
-        getWeatherData: getWeatherData
-        //,getNewsData: getNewsData
+        getWeatherData: getWeatherData,
+        getNewsData: getNewsData
     });
 
 }]);

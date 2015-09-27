bcd13260f4401d65e0a5b7b8b7153cc61291bb3d
Samir Tambe <samir.tambe1@gmail.com>
2015-09-27 00:23:06 -0400

    Changed network service requests to use HttpSvc.getData(OPTIONS) & added reqType to the [weather/req]Params Object

--- js/controllers/Controllers.js
+++ js/controllers/Controllers.js
@@ -1,23 +1,4 @@
-angular.module('TambeTech').controller('StockListCtrl', ['$scope', 'QuandlSvc', '$window', function($scope, QuandlSvc, $window) {
-
-
-    var padWithZero = function (chk) {
-
-        if (chk.length == 1) { return ('0' + chk); }
-
-        else { return chk;  }
-
-    };
-
-
-    $scope.reqParams = {
-        symbol : $scope.stockSymbol,
-        todayDate : new Date(),
-        howLongAgo: new Date(), // just a dummy value, we need it to be a date object
-        duration: '1 Week',
-        startDate: '',
-        endDate: ''
-    };
+angular.module('TambeTech').controller('StockViewCtrl', ['$scope', 'HttpSvc', '$window', function($scope, HttpSvc, $window) {
 
     $scope.investments = [
         {invName: 'Altria Group', invSymbol: 'MO'},
@@ -28,119 +9,131 @@ angular.module('TambeTech').controller('StockListCtrl', ['$scope', 'QuandlSvc', 
         {invName: 'Microsoft', invSymbol: 'MSFT'},
     ];
 
+    $scope.durations = ['1 Week','1 Month','3 Months','6 Months','1 Year','5 Years'];
+
     $scope.reqParams = {
+        reqType: 'stock',
+        symbol : $scope.stockSymbol,
         investment : $scope.investments[0],
         todayDate : new Date(),
         howLongAgo: new Date(), // just a dummy value, we need it to be a date object
-        duration: '1 Week',
+        duration: $scope.durations[0],
         startDate: '',
         endDate: ''
     };
 
-    $scope.durations = ['1 Week',
-                        '1 Month',
-                        '3 Months',
-                        '6 Months',
-                        '1 Year'];
 
     $scope.reqParams.todayDate.setDate($scope.reqParams.todayDate.getDate() - 1);
 
 
-    var createChart = function() {
+    var padWithZero = function (chk) {
 
-        var margin = {top: 20,  right: 20,  bottom: 30,  left: 50 },
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom,
+        if (chk.length == 1) { return ('0' + chk); }
 
-        parseDate = d3.time.format("%Y-%m-%d").parse,
-
-        x = d3.time.scale().range([0, width]),
-
-        y = d3.scale.linear().range([height, 0]),
-
-        xAxis = d3.svg.axis().scale(x).orient("bottom"),
-
-        yAxis = d3.svg.axis().scale(y).orient("left"),
-
-        area = d3.svg.area()
-            .x(function(d) { return x(d.date); })
-            .y0(height)
-            .y1(function(d) { return y(d.close); }),
-
-        svg = d3.select("#stockChartDiv").append("svg")
-        .attr("id","theSVG")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
-
-
-        data = [];
-
-// CONVERT AN LONG ARRAY OF ARRAYS into an array of objects
-
-        for (var cat=0,leng = $scope.graphData.length; cat < leng; cat++) {
-            data.unshift(
-                {
-                    date: $scope.graphData[cat][0],
-                    close: $scope.graphData[cat][1]
-                }
-            );
-        }
-        //console.log('Data Elements = ' + data.length);
-
-        data.forEach(function(d) {
-            d.date = parseDate(d.date);
-            d.close = +d.close;
-        });
-
-        x.domain(d3.extent(data, function(d) { return d.date; }));
-
-        y.domain([0, d3.max(data, function(d) { return d.close; })]);
-
-        svg.append("path")
-          .datum(data)
-          .attr("class", "area")
-          .attr("d", area);
-
-        svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-        svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-            .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text("U.S. Dollars");
-
+        else { return chk;  }
 
     },
+        createChart = function() {
+
+            var margin = {top: 20,  right: 20,  bottom: 30,  left: 50 },
+                width = 960 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom,
+
+            parseDate = d3.time.format("%Y-%m-%d").parse,
+
+            x = d3.time.scale().range([0, width]),
+
+            y = d3.scale.linear().range([height, 0]),
+
+            xAxis = d3.svg.axis().scale(x).orient("bottom"),
+
+            yAxis = d3.svg.axis().scale(y).orient("left"),
+
+            area = d3.svg.area()
+                .x(function(d) { return x(d.date); })
+                .y0(height)
+                .y1(function(d) { return y(d.close); }),
+
+            svg = d3.select("#stockChartDiv").append("svg")
+            .attr("id","theSVG")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
+
+
+            data = [];
+
+    // CONVERT AN LONG ARRAY OF ARRAYS into an array of objects
+
+            for (var cat=0,leng = $scope.graphData.length; cat < leng; cat++) {
+                data.unshift(
+                    {
+                        date: $scope.graphData[cat][0],
+                        close: $scope.graphData[cat][1]
+                    }
+                );
+            }
+            //console.log('Data Elements = ' + data.length);
+
+            data.forEach(function(d) {
+                d.date = parseDate(d.date);
+                d.close = +d.close;
+            });
+
+            x.domain(d3.extent(data, function(d) { return d.date; }));
+
+            y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+            svg.append("path")
+              .datum(data)
+              .attr("class", "area")
+              .attr("d", area);
+
+            svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+            svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+                .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("U.S. Dollars");
+        },
+
     setBeginDate = function() {
+
+        var dur = $scope.durations;
+
         switch($scope.reqParams.duration) {
 
-            case '1 Week':
+            case dur[0]:
                 $scope.reqParams.howLongAgo.setDate($scope.reqParams.todayDate.getDate()-7);
                 break;
 
-            case '1 Month':
+            case dur[1]:
                 $scope.reqParams.howLongAgo.setDate($scope.reqParams.todayDate.getDate()-31);
                 break;
 
-            case '3 Months':
+            case dur[2]:
                 $scope.reqParams.howLongAgo.setDate($scope.reqParams.todayDate.getDate() - 93);
                 break;
 
-            case '6 Months':
+            case dur[3]:
                 $scope.reqParams.howLongAgo.setDate($scope.reqParams.todayDate.getDate()-186);
                 break;
 
-            case '1 Year':
+            case dur[4]:
                 $scope.reqParams.howLongAgo.setDate($scope.reqParams.todayDate.getDate()-366);
+                break;
+
+            case dur[5]:
+                $scope.reqParams.howLongAgo.setDate($scope.reqParams.todayDate.getDate()-1827);
                 break;
 
             default:
@@ -180,7 +173,7 @@ angular.module('TambeTech').controller('StockListCtrl', ['$scope', 'QuandlSvc', 
         }
 
 
-        QuandlSvc.getQuandlData($scope.reqParams)
+        HttpSvc.getData($scope.reqParams)
             .then(function(data) {
 
             $scope.graphData = data;
@@ -199,13 +192,21 @@ angular.module('TambeTech').controller('StockListCtrl', ['$scope', 'QuandlSvc', 
 
         });
     };
-}])
+}]);
 /***********************************************************************************************/
 
-/***********************************************************************************************/
-.controller('WeatherCtrl', ['$scope', 'WeatherSvc', function($scope, WeatherSvc) {
 
-    $scope.weatherParams = { forecastType: "/conditions"};
+
+
+
+
+/***********************************************************************************************/
+angular.module('TambeTech').controller('WeatherCtrl',['$scope','HttpSvc',function($scope, HttpSvc) {
+
+    $scope.weatherParams = {
+        forecastType: '/conditions',
+        reqType: 'weather'
+    };
 
     $scope.locations = [
         {displayName: "Atlanta, Georgia", stateCityStr: "GA/Atlanta"},
@@ -230,7 +231,7 @@ angular.module('TambeTech').controller('StockListCtrl', ['$scope', 'QuandlSvc', 
 
     $scope.getWeather = function() {
 
-        WeatherSvc.getWeatherData($scope.weatherParams).then(function(result) {
+        HttpSvc.getData($scope.weatherParams).then(function(result) {
             $scope.weather = result.current_observation;
         }).catch(function(error) {
             console.log("WeatherCtrl - Current Conditions - Catch: " + error);
@@ -241,7 +242,7 @@ angular.module('TambeTech').controller('StockListCtrl', ['$scope', 'QuandlSvc', 
         $scope.weatherParams.forecastType = "/forecast";
 
 
-        WeatherSvc.getWeatherData($scope.weatherParams).then(function(result) {
+        HttpSvc.getData($scope.weatherParams).then(function(result) {
             $scope.threeForecast = result.forecast.simpleforecast.forecastday;
         }).catch(function(error) {
             console.log("WeatherCtrl - 3 Day Forecast - Catch: " + error);
@@ -251,4 +252,15 @@ angular.module('TambeTech').controller('StockListCtrl', ['$scope', 'QuandlSvc', 
         $scope.weatherParams.forecastType = "/conditions";
 
     };// getWeather()
+}]);
+/***********************************************************************************************/
+
+
+
+
+
+
+/***********************************************************************************************/
+angular.module('TambeTech').controller('HomeCtrl',['$scope', 'HttpSvc', '$window', function($scope, HttpSvc, $window) {
+
 }]);

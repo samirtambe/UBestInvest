822ef2c4dbc89ae5afa73490d03bb3a74b2678c1
Samir Tambe <samir.tambe1@gmail.com>
2015-09-30 22:24:09 -0400

    Added code to read query type based on ctype attribute, added choice for Dow Jones and SP500

--- js/directives.js
+++ js/directives.js
@@ -1,94 +1,122 @@
-angular.module('TambeTech').directive('stockGraph', ['$parse', '$window', function($parse, $window) {
+angular.module('TambeTech').directive('stockGraph', [function() {
 
    return {
        restrict: 'E',
-       template: "<div id='mktDivDowJOnes' width='400' height='200'></div>",
+       scope: {
+           ctype: '@'
+       },
+       template: '<div class="frontChartDiv"></div>',
+       controller: 'MarketCtrl',
        link: function(scope, elem, attrs) {
-
-           var exp = $parse(attrs.chartData),
-
-               grData = exp(scope);
-
-
-           var margin = {top: 10, right: 10, bottom: 30, left: 55},
-
-               width = 420 - margin.left - margin.right,
-
-               height = 220 - margin.top - margin.bottom,
-
-               parseDate = d3.time.format("%Y-%m-%d").parse,
-
-
-               // create x-axis scale
-               x = d3.time.scale().range([0, width]),
-
-               // create y-axis scale
-               y = d3.scale.linear().range([height, 0]),
-
-               // orient x-axis
-               xAxis = d3.svg.axis().scale(x).orient("bottom"),
-
-               // orient y-axis
-               yAxis = d3.svg.axis().scale(y).orient("left"),
-
-               area = d3.svg.area().x(function(d) {
-                   return x(d.date);
-
-               }).y0(height).y1(function(d) {
-                   return y(d.close);
-               }),
-
-               svg = d3.select("#mktDivDowJOnes")
-                        .append("svg")
-                        .attr("id","mktDowJonesSVG")
-                        .attr("width", width + margin.left + margin.right)
-                        .attr("height", height + margin.top + margin.bottom)
-                        .append("g")
-                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
            var drawChart = function() {
 
-               var data = [];
+               var chrtDivs = document.getElementsByClassName('frontChartDiv');
 
-    // CONVERT AN LONG ARRAY OF ARRAYS into an array of objects
+               var aChartDiv = '';
 
-               for (var cat = 0; cat < grData.length; cat++) {
-                        data.unshift(
-                            {
-                                date: grData[cat][0],//$scope.graphData[cat][0],
-                                close: grData[cat][1] //$scope.graphData[cat][1]
-                            }
-                        );
-                    }
+               if (attrs.ctype == 'dowjones') {
+                   aChartDiv = chrtDivs[0];
+                   aChartDiv.id = 'frontMktDowJonesDiv';
+               }
+               else if(attrs.ctype == 'sp500') {
+                   aChartDiv = chrtDivs[1];
+                   aChartDiv.id = 'frontMktSP500Div';
+               }
 
-               data.forEach(function(d) {
-                   d.date = parseDate(d.date);
-                   d.close = +d.close;
-               });
+               var wide = aChartDiv.scrollWidth,
 
-               x.domain(d3.extent(data, function(d) { return d.date; } ) );
+                   tall = aChartDiv.scrollHeight,
 
-               y.domain([0, d3.max(data, function(d) { return d.close; } ) ] );
+                   margin = {top: 10, right: 10, bottom: 30, left: 55},
 
-               svg.append("path").datum(data).attr("class", "area").attr("d", area);
+                   width = 420 - margin.left - margin.right,
 
-               // Drawing x-axis
-               svg.append("g").attr("class","x axis").attr("transform","translate(0,"+height+")")
-                   .call(xAxis);
+                   height = 220 - margin.top - margin.bottom,
 
-               // Drawing y-axis
-               svg.append("g").attr("class", "y axis").call(yAxis).append("text")
-                   .attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em")
-                   .style("text-anchor", "end").text("U.S. Dollars");
+                   parseDate = d3.time.format("%Y-%m-%d").parse,
+
+                   // create x-axis scale
+                   x = d3.time.scale().range([0, width]),
+
+                   // create y-axis scale
+                   y = d3.scale.linear().range([height, 0]),
+
+                   // orient x-axis
+                   xAxis = d3.svg.axis().scale(x).orient("bottom"),
+
+                   // orient y-axis
+                   yAxis = d3.svg.axis().scale(y).orient("left"),
+
+                   area = d3.svg.area().x(function(d) {
+                       return x(d.date);
+
+                   }).y0(height).y1(function(d) {
+                       return y(d.close);
+                   }),
+
+                   svg = d3.select('#'+aChartDiv.id)
+                            .append("svg")
+                            .attr("width", width + margin.left + margin.right)
+                            .attr("height", height + margin.top + margin.bottom)
+                            .append("g")
+                            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+                   var data = [];
+
+        // CONVERT AN LONG ARRAY OF ARRAYS into an array of objects
+
+                   for (var cat = 0; cat < grData.length; cat++) {
+                            data.unshift(
+                                {
+                                    date: grData[cat][0],//$scope.graphData[cat][0],
+                                    close: grData[cat][1] //$scope.graphData[cat][1]
+                                }
+                            );
+                        }
+
+                   data.forEach(function(pair) {
+                       pair.date = parseDate(pair.date);
+                       pair.close = +pair.close;
+                   });
+
+                   x.domain(d3.extent(data, function(d) { return d.date; } ) );
+
+                   y.domain([0, d3.max(data, function(d) { return d.close; } ) ] );
+
+                   svg.append("path").datum(data).attr("class", "area").attr("d", area);
+
+                   // Drawing x-axis
+                   svg.append("g").attr("class","x axis").attr("transform","translate(0,"+height+")")
+                       .call(xAxis);
+
+                   // Drawing y-axis
+                   svg.append("g").attr("class", "y axis").call(yAxis).append("text")
+                       .attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em")
+                       .style("text-anchor", "end").text("U.S. Dollars");
 
            };//drawChart
 
-           scope.$watchCollection(exp, function(newVal, oldVal) {
-console.log('change');
-               grData = newVal;
-               drawChart();
-           });
+           var grData = [];
 
+           if (attrs.ctype == 'dowjones') {
+               scope.populateDowJones();
+           }
+           else if(attrs.ctype == 'sp500') {
+               scope.populateSP500();
+           }
+
+
+
+           // Remember watch does not go very deep it only checks reference
+           // 'graphData' refers to $scope.graphData in the MarketCtrl
+           scope.$watch('graphData', function(newVal) {
+               if (newVal) {
+                   grData = newVal;
+                   drawChart();
+               }
+           });
        }// LINK FUNCTION
    };// RETURN
 }]);

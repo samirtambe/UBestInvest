05497a0a665811541935f8b0a2783a94f6fd8d9c
Samir Tambe <samir.tambe1@gmail.com>
2015-10-12 15:17:24 -0400

    Removed companyListModal directive

--- js/directives.js
+++ js/directives.js
@@ -158,34 +158,3 @@ angular.module('UBestInvest').directive('errorModal', [function() {
         }
     };
 }]);
-
-angular.module('UBestInvest').directive('companyListModal', [function() {
-    return {
-        templateUrl: 'views/compListModal.html',
-        restrict: 'E',
-        replace:true,
-        scope:true,
-        link: function postLink(scope, element, attrs) {
-
-            scope.$watch(attrs.visible, function(value) {
-
-                if(value == true) { $(element).modal('show'); }
-                else { $(element).modal('hide'); }
-            });
-
-            $(element).on('shown.bs.modal', function() {
-
-                scope.$apply(function() {
-
-                    scope.$parent[attrs.visible] = true;
-                });
-            });
-
-            $(element).on('hidden.bs.modal', function(){
-                scope.$apply(function() {
-                    scope.$parent[attrs.visible] = false;
-                });
-            });
-        }
-    };
-}]);

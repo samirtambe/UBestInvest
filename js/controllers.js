87805c1a7a4a1612f4d09abfa27d5f6af9bc9ffd
Samir Tambe <samir.tambe1@gmail.com>
2015-12-10 21:41:11 -0500

    Added innerTickSize(...) to the axises

--- js/controllers.js
+++ js/controllers.js
@@ -64,9 +64,9 @@ angular.module('UBestInvest').controller('ResearchCtrl', ['$scope', 'HttpSvc', '
 
             y = d3.scale.linear().range([height, 0]),
 
-            xAxis = d3.svg.axis().scale(x).orient("bottom"),
+            xAxis = d3.svg.axis().scale(x).orient("bottom").innerTickSize(-height),
 
-            yAxis = d3.svg.axis().scale(y).orient("left"),
+            yAxis = d3.svg.axis().scale(y).orient("left").innerTickSize(-width),
 
             area = d3.svg.area()
                 .x(function(d) {

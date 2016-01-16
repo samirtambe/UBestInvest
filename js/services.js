a0b971690fdca9f22e446b596675a8d81d3eaaea
Samir Tambe <samir.tambe1@gmail.com>
2016-01-15 23:38:44 -0500

    Added Spinner Code as a Service

--- js/services.js
+++ js/services.js
@@ -340,10 +340,46 @@ angular.module('UBestInvest').service('ChartSvc', [function() {
 
 
 /***********************************************************************************************/
-angular.module('UBestInvest').service('ToJsonSvc', [function() {
-    //delete first 16 chars (quandl code,name)
-    //get rid of WIKI/ for each name value pair
+angular.module('UBestInvest').service('SpinSvc', [function() {
 
-    // USE http://jsfiddle.net/sturtevant/AZFvQ/ as your inspiration
+//    This section creates the spinner visual to indicate that the page is processing
+//    or that the content is still loading.
 
+
+    function start(spinner) {
+
+        var spinOptions = {
+            lines: 13, // The number of lines to draw
+            length: 19, // The length of each line
+            width: 8, // The line thickness
+            radius: 28, // The radius of the inner circle
+            scale: 0.75, // Scales overall size of the spinner
+            corners: 1, // Corner roundness (0..1)
+            color: '#000', // #rgb or #rrggbb or array of colors
+            opacity: 0.2, // Opacity of the lines
+            rotate: 0, // The rotation offset
+            direction: 1, // 1: clockwise, -1: counterclockwise
+            speed: 1, // Rounds per second
+            trail: 25, // Afterglow percentage
+            fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+            zIndex: 2e9, // The z-index (defaults to 2000000000)
+            className: 'spinner', // The CSS class to assign to the spinner
+            top: '50%', // Top position relative to parent
+            left: '50%', // Left position relative to parent
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            position: 'absolute' // Element positioning
+        },
+        target = document.getElementById('theBodyTag'),
+        spinner = new Spinner(spinOptions).spin(target);
+    }
+
+    function stop(spinner) {
+        spinner.stop();
+    }
+
+    return ({
+        startSpinning: start,
+        stopSpinning: stop
+    });
 }]);

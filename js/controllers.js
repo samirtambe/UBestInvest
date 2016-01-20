b6dee36433b46ba272c64955956a3784e65d1338
Samir Tambe <samir.tambe1@gmail.com>
2016-01-20 16:55:05 -0500

    commented out hardcoded value for opts, now referencing SpinnerSvc

--- js/controllers.js
+++ js/controllers.js
@@ -243,29 +243,29 @@ angular.module('UBestInvest').controller('WeatherCtrl',['$scope','HttpSvc',funct
 angular.module('UBestInvest').controller('MarketCtrl', ['$scope', 'ChartSvc', 'HttpSvc', 'SpinnerSvc',
         function($scope, ChartSvc, HttpSvc, SpinnerSvc) {
 
-    var opts = {
-        lines: 11 // The number of lines to draw
-        , length: 20 // The length of each line
-        , width: 14 // The line thickness
-        , radius: 40 // The radius of the inner circle
-        , scale: 0.5 // Scales overall size of the spinner
-        , corners: 1 // Corner roundness (0..1)
-        , color: '#000' // #rgb or #rrggbb or array of colors
-        , opacity: 0.05 // Opacity of the lines
-        , rotate: 0 // The rotation offset
-        , direction: 1 // 1: clockwise, -1: counterclockwise
-        , speed: 1 // Rounds per second
-        , trail: 60 // Afterglow percentage
-        , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-        , zIndex: 2e9 // The z-index (defaults to 2000000000)
-        , className: 'spinner' // The CSS class to assign to the spinner
-        , top: '50%' // Top position relative to parent
-        , left: '50%' // Left position relative to parent
-        , shadow: false // Whether to render a shadow
-        , hwaccel: false // Whether to use hardware acceleration
-        , position: 'absolute' // Element positioning
-    };
-    var target = document.getElementById('theBodyTag');
+//    var opts = {
+//        lines: 11 // The number of lines to draw
+//        , length: 20 // The length of each line
+//        , width: 14 // The line thickness
+//        , radius: 40 // The radius of the inner circle
+//        , scale: 0.5 // Scales overall size of the spinner
+//        , corners: 1 // Corner roundness (0..1)
+//        , color: '#000' // #rgb or #rrggbb or array of colors
+//        , opacity: 0.05 // Opacity of the lines
+//        , rotate: 0 // The rotation offset
+//        , direction: 1 // 1: clockwise, -1: counterclockwise
+//        , speed: 1 // Rounds per second
+//        , trail: 60 // Afterglow percentage
+//        , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+//        , zIndex: 2e9 // The z-index (defaults to 2000000000)
+//        , className: 'spinner' // The CSS class to assign to the spinner
+//        , top: '50%' // Top position relative to parent
+//        , left: '50%' // Left position relative to parent
+//        , shadow: false // Whether to render a shadow
+//        , hwaccel: false // Whether to use hardware acceleration
+//        , position: 'absolute' // Element positioning
+//    };
+//    var target = document.getElementById('theBodyTag');
 
     $scope.durations = ['1 Week','1 Month','3 Months','6 Months','1 Year','5 Years'];
 
@@ -296,7 +296,8 @@ angular.module('UBestInvest').controller('MarketCtrl', ['$scope', 'ChartSvc', 'H
 
     $scope.populateDowJones = function() {
 
-    var spinner = new Spinner(opts).spin(target);
+    //var spinner = new Spinner(opts).spin(target);
+        var spinner = SpinnerSvc.getSpinner();
 
         HttpSvc.getDowJonesData($scope.reqParams).then(function(data) {
 

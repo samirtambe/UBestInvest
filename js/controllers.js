ae066a41472d1e36ea7341dc82cc4c968308d3c1
Samir Tambe <samir.tambe1@gmail.com>
2016-01-20 17:12:07 -0500

    added spinner to researchctrl

--- js/controllers.js
+++ js/controllers.js
@@ -8,8 +8,6 @@ angular.module('UBestInvest').controller('ResearchCtrl', ['$scope', 'HttpSvc', '
 
     $scope.durations = ['1 Week','1 Month','3 Months','6 Months','1 Year','5 Years'];
 
-    var spinnerObject;
-
     $scope.reqParams = {
 
         pair: {symbol : undefined, name : undefined },
@@ -119,12 +117,13 @@ angular.module('UBestInvest').controller('ResearchCtrl', ['$scope', 'HttpSvc', '
     $scope.getGraph = function(validForm) {
 
         if (validForm == true) {
-//console.log('calculating begin date svc'+ChartSvc.calcBeginDate($scope));
+
+            var spinner = SpinnerSvc.getSpinner();//starts spinning
+
             $scope.reqParams.howLongAgo.setDate(ChartSvc.calcBeginDate($scope));
 
             var tframe = ChartSvc.formatDateShort($scope);
-//console.log('startdate = '+ tframe.start);
-//console.log('enddate = '+ tframe.end);
+
             $scope.reqParams.startDate = tframe.start;
 
             $scope.reqParams.endDate = tframe.end;
@@ -156,6 +155,7 @@ angular.module('UBestInvest').controller('ResearchCtrl', ['$scope', 'HttpSvc', '
 
                 $scope.errorModalDetails = errObj.details;
                 $scope.showErrModal = true;
+                spinner.stop();
 
             }).finally(function() {
 
@@ -163,6 +163,7 @@ angular.module('UBestInvest').controller('ResearchCtrl', ['$scope', 'HttpSvc', '
                 $scope.reqParams.howLongAgo = new Date();
                 $scope.reqParams.startDate = '';
                 $scope.reqParams.endDate = '';
+                spinner.stop();
 
             });
         }// if formValid
@@ -242,30 +243,6 @@ angular.module('UBestInvest').controller('WeatherCtrl',['$scope','HttpSvc',funct
 /***********************************************************************************************/
 angular.module('UBestInvest').controller('MarketCtrl', ['$scope', 'ChartSvc', 'HttpSvc', 'SpinnerSvc',
         function($scope, ChartSvc, HttpSvc, SpinnerSvc) {
-
-//    var opts = {
-//        lines: 11 // The number of lines to draw
-//        , length: 20 // The length of each line
-//        , width: 14 // The line thickness
-//        , radius: 40 // The radius of the inner circle
-//        , scale: 0.5 // Scales overall size of the spinner
-//        , corners: 1 // Corner roundness (0..1)
-//        , color: '#000' // #rgb or #rrggbb or array of colors
-//        , opacity: 0.05 // Opacity of the lines
-//        , rotate: 0 // The rotation offset
-//        , direction: 1 // 1: clockwise, -1: counterclockwise
-//        , speed: 1 // Rounds per second
-//        , trail: 60 // Afterglow percentage
-//        , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-//        , zIndex: 2e9 // The z-index (defaults to 2000000000)
-//        , className: 'spinner' // The CSS class to assign to the spinner
-//        , top: '50%' // Top position relative to parent
-//        , left: '50%' // Left position relative to parent
-//        , shadow: false // Whether to render a shadow
-//        , hwaccel: false // Whether to use hardware acceleration
-//        , position: 'absolute' // Element positioning
-//    };
-//    var target = document.getElementById('theBodyTag');
 
     $scope.durations = ['1 Week','1 Month','3 Months','6 Months','1 Year','5 Years'];
 

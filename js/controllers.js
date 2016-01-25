e55b5a874d9d582b003338dcdaeb1bdb60704dc7
Samir Tambe <samir.tambe1@gmail.com>
2016-01-25 00:59:27 -0500

    Removed createChart function

--- js/controllers.js
+++ js/controllers.js
@@ -27,105 +27,6 @@ angular.module('UBestInvest').controller('ResearchCtrl',
 
     $scope.reqParams.todayDate.setDate($scope.reqParams.todayDate.getDate() - 1);
 
-
-    var createChart = function() {
-
-        var stkChrtDiv = document.getElementById("stockChartDiv"),
-            wide = stkChrtDiv.scrollWidth,
-            tall = stkChrtDiv.scrollHeight;
-
-        var margin = {
-            top: 20,
-            right: 20,
-            bottom: 80,
-            left: 50
-        },
-            width = wide - margin.left - margin.right,
-            height = tall - margin.top - margin.bottom,
-
-            parseDate = d3.time.format("%Y-%m-%d").parse,
-
-            x = d3.time.scale().range([0, width]),
-
-            y = d3.scale.linear().range([height, 0]),
-
-            xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5),
-
-            yAxis = d3.svg.axis().scale(y).orient("left").ticks(5).innerTickSize(-width),
-
-            area = d3.svg.area()
-                .x(function(d) {
-                    return x(d.date);
-
-                }).y0(height)
-
-                .y1(function(d) {
-                    return y(d.close);
-                }),
-
-            svg = d3.select("#stockChartDiv")
-                    .append("svg")
-                    .attr("id","theSVG")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                    .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
-
-
-            data = [];
-
-    // CONVERT AN LONG ARRAY OF ARRAYS into an array of objects
-
-            for (var cat=0,leng = $scope.graphData.length; cat < leng; cat++) {
-                data.unshift(
-                    {
-                        date: $scope.graphData[cat][0],
-                        close: $scope.graphData[cat][1]
-                    }
-                );
-            }
-
-            data.forEach(function(d) {
-                d.date = parseDate(d.date);
-                d.close = +d.close;
-            });
-
-            x.domain(d3.extent(data, function(d) { return d.date; }));
-
-            y.domain([d3.min(data,  function(d) { return d.close; } )
-                      ,
-                      d3.max(data, function(d) { return d.close; })
-                     ]);
-
-            svg.append("path")
-                .datum(data)
-                .attr("class", "area")
-                .attr("d", area);
-
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis)
-                .selectAll("text")
-                .style("text-anchor", "end")
-                .attr("dx", "-.8em")
-                .attr("dy", ".15em")
-                .attr("transform", function(d) {
-                    return "rotate(-45)";
-                    });
-
-
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("U.S. Dollars");
-        };
-
     $scope.getGraph = function(validForm) {
 
         if (validForm == true) {
@@ -151,14 +52,13 @@ angular.module('UBestInvest').controller('ResearchCtrl',
             catch(error) {}
 
 
-            HttpSvc.getStockData($scope.reqParams)
-                .then(function(dataset) {
+            HttpSvc.getStockData($scope.reqParams).then(function(dataset) {
 
                 $scope.graphData = dataset.data;
+
                 $scope.reqParams.pair.name = dataset.name;
 
                 GraphSvc.createGraph('stocks', $scope.graphData);
-                //createChart();
 
             }).catch(function(errObj) {
 

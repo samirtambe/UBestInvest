e8df90a7f79853dae8af76883d08c315fad1db47
Samir Tambe <samir.tambe1@gmail.com>
2016-01-29 18:04:51 -0500

    Added anti-scroll code

--- js/app.js
+++ js/app.js
@@ -30,41 +30,8 @@ angular.module('UBestInvest',['ui.router'])
         templateUrl:'views/finance.html',
         controller:'ResearchCtrl'
     });
-});
-/*
-// OLD APP.JS
-angular.module('UBestInvest',['ui.router']).config(
 
-    function($stateProvider, $urlRouterProvider) {
-
-    $urlRouterProvider.otherwise('/home');
-
-    $stateProvider
-        .state('home', {
-            url:'/home',
-            views: {
-                '': {
-                    templateUrl:'views/home.html'
-                },
-            'news@home': {
-                templateUrl: 'views/news.html'
-            },
-            'markets@home': {
-                templateUrl: 'views/markets.html'
-            }
-        }
-    }).state('about',{
-        url:'/about',
-        templateUrl:'views/about.html'
-
-    }).state('weather',{
-        url:'/weather',
-        templateUrl:'views/weather.html',
-        controller:'WeatherCtrl'
-    }).state('research',{
-        url:'/research',
-        templateUrl:'views/finance.html',
-        controller:'ResearchCtrl'
+    $rootScope.$on('$stateChangeSuccess', function() {
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
     });
 });
-*/

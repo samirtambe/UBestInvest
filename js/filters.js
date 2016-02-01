0c9fd56bce1e88fb8f64af8d014f012503f9460b
Samir Tambe <samir.tambe1@gmail.com>
2016-02-01 09:39:43 -0500

    spacing

--- js/filters.js
+++ js/filters.js
@@ -1,11 +1,9 @@
 angular.module('UBestInvest').filter('startFrom', function() {
 
-    function slicer(input, start) {
+    return function(input, start) {
+
         start = +start; //parse to int
+
         return input.slice(start);
     }
-
-    return {
-        slicer: slicer
-    };
 });

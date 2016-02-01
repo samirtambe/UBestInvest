7bbc58322af1657aae4321bc24f4431851fab704
Samir Tambe <samir.tambe1@gmail.com>
2016-02-01 14:07:23 -0500

    Removed Debug statements

--- js/filters.js
+++ js/filters.js
@@ -1,9 +1,9 @@
 angular.module('UBestInvest').filter('startFrom', function() {
 
     return function(input, start) {
-console.log("input.length = "+input.length);
+
         start = +start; //parse to int
-        console.log("start = "+start);
+
         return input.slice(start);
     }
 });

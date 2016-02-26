4ce78853fde66162970920183ac71c93720d7948
Samir Tambe <samir.tambe1@gmail.com>
2016-02-26 12:05:48 -0500

    added learn state

--- js/app.js
+++ js/app.js
@@ -33,6 +33,11 @@ angular.module('UBestInvest',['ui.router'])
     }).state('terms', {
         url: '/terms',
         templateUrl: 'views/terms.html'
+
+    }).state('learn',{
+        url: '/learn',
+        templateUrl: 'views/learn.html',
+        controller: 'LearnCtrl'
     });
 
 });

dc7abfe8f9e29e46b9db110bfa99e92c163c8181
Samir Tambe <samir.tambe1@gmail.com>
2016-03-07 23:50:55 -0500

    rewired so that marketcharts show on homepage

--- js/app.js
+++ js/app.js
@@ -12,6 +12,41 @@ angular.module('UBestInvest',['ui.router','firebase'])
         templateUrl:'views/about.html'
 
     }).state('news', {
+        url: '/news',
+        templateUrl: 'views/news.html',
+        controller: 'BizNewsCtrl'
+
+    }).state('research',{
+        url:'/research',
+        templateUrl:'views/finance.html',
+        controller:'ResearchCtrl'
+
+    }).state('terms', {
+        url: '/terms',
+        templateUrl: 'views/terms.html'
+
+    }).state('learn',{
+        url: '/learn',
+        templateUrl: 'views/learn.html',
+        controller: 'LearnCtrl'
+    });
+});
+/*
+// OLD ONE that worked
+angular.module('UBestInvest',['ui.router','firebase'])
+    .config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider.state('home', {
+        url: '/home',
+        templateUrl: 'views/home.html'
+
+    }).state('about', {
+        url:'/about',
+        templateUrl:'views/about.html'
+
+    }).state('news', {
          url: '/news',
          views: {
             '': {
@@ -41,3 +76,5 @@ angular.module('UBestInvest',['ui.router','firebase'])
     });
 
 });
+
+*/

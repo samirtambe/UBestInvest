70b82ec91982b7e054bc87289e9035ba78690d16
Samir Tambe <samir.tambe1@gmail.com>
2016-03-09 18:42:59 -0500

    Spacing

--- js/controllers.js
+++ js/controllers.js
@@ -200,7 +200,7 @@ angular.module('UBestInvest').controller('NewsBoxCtrl', ['$scope', 'HttpSvc', 'S
         numOfArticles = undefined;
 
     $scope.currentPage = 0;
-console.log('NewsBoxCtrl...');
+
     $scope.pageSize = 6;
 
     $scope.populateNewsBox = function(newsType) {
@@ -251,7 +251,9 @@ console.log('NewsBoxCtrl...');
 
         }).finally(function() {
 
-            $scope.numberOfPages = function() {return Math.ceil(numOfArticles/$scope.pageSize);};
+            $scope.numberOfPages = function() {
+                return Math.ceil(numOfArticles/$scope.pageSize);
+            };
 
             spinner.stop();
         });
